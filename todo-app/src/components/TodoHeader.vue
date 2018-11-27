778a2a63dce64c15fabd8acecf20a154f1891683
songjein <jeinsong100@gmail.com>
2018-11-27 16:43:57 +0000

    todo example

--- todo-app/src/components/TodoHeader.vue
+++ todo-app/src/components/TodoHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		header
+		<h1>Todo It!</h1>
 	</div>
 </template>
 
@@ -10,6 +10,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+	color: green;
+	font-weight: 900;
+	margin: 2.5rem 0 1.5rem; 
+}
 </style>
 
--- todo-app/src/components/TodoInput.vue
+++ todo-app/src/components/TodoInput.vue
@@ -1,16 +1,59 @@
 <template>
 	<!-- component당 root element는 하나만 --> 
-	<div>
-		input
+	<div class="inputBox shadow">
+		<input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+		<span class="addContainer" v-on:click="addTodo">
+			<i class="fas fa-plus addBtn"></i>
+		</span>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data: function() {
+		return {
+			newTodoItem: ""	
+		}	
+	},
+	methods: {
+		addTodo: function() {
+			if (this.newTodoItem !== '') {
+				var obj = {completed: false, item: this.newTodoItem};
+				localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+				this.clearInput();
+			}
+		},
+		clearInput: function() {
+			this.newTodoItem = "";	
+		}
+	}
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+	outline: none;
+}
+.inputBox {
+	background: white;
+	height: 50px;
+	line-height: 50px;
+	border-radius: 5px;
+}
+.inputBox input {
+	border-style: none;
+	font-size: 0.9rem;
+}
+.addContainer {
+	float: right;
+	background: linear-gradient(right, #6478FB, #8763FB);
+	display: block;
+	width: 3rem;
+	border-radius: 0 5px 5px 0;
+}
+.addBtn {
+	color: white;
+	vertical-align: middle;
+}
 </style>
 
